7d3129182dfcfaf75f4b7686cb37dc1484628b3c
Diego Reis <diego.luis@lavid.ufpb.br>
2021-08-03 15:21:28 -0300

    DoubleLinked list

---
+++ Assets/Scripts/Util/DoubleLinkedList/DoubleLinkedList.cs
@@ -0,0 +1,52 @@
+﻿namespace Lavid.Libraske.DataStruct
+{
+    public class DoubleLinkedList<T>
+    {
+        private DoubleLinkedNode<T> _current;
+
+        public void AddItem(T item)
+        {
+            DoubleLinkedNode<T> temp = new DoubleLinkedNode<T>();
+
+            _current._next = temp;
+            temp._previous = _current;
+
+            temp._value = item;
+
+            _current = temp;
+        }
+
+        public bool HasNext() => _current.HasNext();
+        public bool HasPrevious() => _current.HasPrevious();
+
+        public T GetFirst()
+        {
+            DoubleLinkedNode<T> first = _current;
+
+            while (first.HasPrevious())
+                first = first.GetPrevious();
+
+            return first.GetValue();
+        }
+        public T GetLast()
+        {
+            DoubleLinkedNode<T> last = _current;
+
+            while (last.HasNext())
+                last = last.GetNext();
+
+            return last.GetValue();
+        }
+        public T GetCurrent() => _current.GetValue();
+        public T GetNext()
+        {
+            _current = _current.GetNext();
+            return _current.GetValue();
+        }
+        public T GetPrevious()
+        {
+            _current = _current.GetPrevious();
+            return _current.GetValue();
+        }
+    }
+}
