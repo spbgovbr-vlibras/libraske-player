c0bc6e86546bacd59426d0a62d8b51e7753907d2
Diego Reis <diego.luis@lavid.ufpb.br>
2021-11-22 20:54:45 -0300

    video player async fix

--- Assets/Scripts/Video/VideoController.cs
+++ Assets/Scripts/Video/VideoController.cs
@@ -1,18 +1,35 @@
 using UnityEngine;
-using System.IO;
 using UnityEngine.Video;
+using System.Collections;
 
 public class VideoController : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
     public string startingFile;
+
     void Start()
     {
         PlayVideo(startingFile);
     }
+
     public void PlayVideo(string filename)
     {
-        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, filename);
+        string url = System.IO.Path.Combine(Application.streamingAssetsPath, filename);
+        StartCoroutine(PlayVideoCoroutine(url));
+    }
+
+    private IEnumerator PlayVideoCoroutine(string url)
+    {
+        if (videoPlayer == null)
+            yield break;
+
+        videoPlayer.source = UnityEngine.Video.VideoSource.Url;
+        videoPlayer.url = url;
+        videoPlayer.Prepare();
+
+        while (!videoPlayer.isPrepared)
+            yield return null;
+
         videoPlayer.Play();
     }
 }
